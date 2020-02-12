a0453f118d0362d3fcefbc9def891a633a3c50fb
bloodforcream <51474632+bloodforcream@users.noreply.github.com>
2020-02-12 17:12:19 +0300

    Update settings.py

--- property/settings.py
+++ property/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*ffjo6ck%7rercq5n0($s8g*1$#xcn^x1#y=i7yoon=5$(8kyu'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -23,7 +23,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'crispy_forms',
+    
     'flats',
 ]
 
@@ -61,25 +63,14 @@ WSGI_APPLICATION = 'property.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'propertydb',
-#         'USER': 'dreamanalytics',
-#         'PASSWORD': 'ubsfuhi1231asA#',
-#         'HOST': 'propertydb.cwewq89nxqux.us-west-2.rds.amazonaws.com',
-#         'PORT': '5432',
-#
-#     }
-# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'new_flatsDB',
-        'USER': 'postgres',
-        'PASSWORD': 'qwerty123',
-        'HOST': 'localhost',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '5432',
 
     }
